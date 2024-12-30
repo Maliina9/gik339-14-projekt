e101ea4fa1e2e75a7ca3a1f50082b55fe83cd9b1
Maliina9 <Maliina9991@outlook.com>
2024-12-30 11:18:03 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,1 +1,18 @@
-// Here ya go Maliina :)
+// Here ya go Maliina :) 
+//Tack åke
+
+const express = require('express');
+const server = express(); 
+const sqlite3 = require('sqlite3').verbose();
+
+server
+  .use(express.json())
+  .use(express.urlencoded({ extended: false }))
+  .use((req, res, next) => {
+    res.header('Acccess-Control-Allow-Origin', '*');
+    res.header('Allow-Control-Allow-Headers', '*');
+    res.header('Access-Control-Allow-Methods', '*');
+    next();
+  });
+
+  
